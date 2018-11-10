eb70f8295e65ed604d399b0faa482dfe720c18aa
j- <j@skeoh.com>
2018-11-10 22:35:58 +1100

    Set video ref when rendering camera stream

--- src/containers/CameraStream.ts
+++ src/containers/CameraStream.ts
@@ -1,5 +1,6 @@
 import { connect, MapStateToProps, MapDispatchToProps, MergeProps } from 'react-redux';
 import Video, { Props as P } from '../components/Video';
+import { setVideoRef } from '../store/actions';
 
 import {
 	RootReducerState,
@@ -8,13 +9,21 @@ import {
 	getCameraUrl,
 } from '../store';
 
+interface DispatchProps extends P {
+	ref: (el: HTMLVideoElement) => void;
+}
+
 const mapStateToProps: MapStateToProps<P, P, RootReducerState> = (state) => ({
 	width: getScreenInnerWidth(state),
 	height: getScreenInnerHeight(state),
 	src: getCameraUrl(state)!,
 });
 
-const mapDispatchToProps: MapDispatchToProps<P, P> = {};
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, P> = (dispatch) => ({
+	ref: (el) => {
+		dispatch(setVideoRef(el));
+	},
+});
 
 const mergeProps: MergeProps<P, P, P, P> = (state, dispatch, own) => ({
 	...state,
