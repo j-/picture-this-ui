786ecf2e389b841e9f0f371e143b5cd2e28f7fd9
j- <j@skeoh.com>
2020-06-06 19:56:37 +1000

    Print device counts

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -7,6 +7,10 @@ export interface Props {
 const Devices: React.FC<Props> = ({ devices }) => {
   return (
     <ul>
+      <li>
+        <strong>{devices.length} device(s).</strong>{' '}
+        <strong>{devices.filter((device) => device.kind === 'videoinput').length} video device(s).</strong>
+      </li>
       {devices.map((device, i) => (
         <li key={i}>
           <pre>{JSON.stringify(device)}</pre>
