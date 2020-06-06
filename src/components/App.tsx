a7ee5fa13b2616a722c4d99c644e2d3d9240292e
j- <j@skeoh.com>
2020-06-06 17:37:50 +1000

    Add diagnostics to UI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCameraURL, getCameraError } from '../store';
+import { requestCamera } from '../store/action-request-camera';
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const cameraURL = useSelector(getCameraURL);
+  const cameraError = useSelector(getCameraError);
   return (
-    <div className="App" />
+    <div className="App">
+      <dl>
+        <dt>Camera URL</dt>
+        <dd><code>{cameraURL || <em>N/A</em>}</code></dd>
+      </dl>
+      <dl>
+        <dt>Camera error</dt>
+        <dd><code>{cameraError || <em>N/A</em>}</code></dd>
+      </dl>
+      <button type="button" onClick={() => dispatch(requestCamera())}>
+        Request camera
+      </button>
+    </div>
   );
 }
 
