8b6ed85979a680a7df5bc6ebc3d78de6ff6029a3
j- <j@skeoh.com>
2020-06-08 01:01:41 +1000

    Add exact scale calculation

--- src/capture.ts
+++ src/capture.ts
@@ -3,10 +3,19 @@ const ctx = canvas.getContext('2d');
 
 export const captureImageFromVideo = async (video: HTMLVideoElement) => {
   if (!ctx) throw new Error('Expected rendering context');
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  ctx.drawImage(video, 0, 0);
-  const capture = new Image(video.videoWidth, video.videoHeight);
+  const size = Math.min(video.videoWidth, video.videoHeight);
+  canvas.width = size;
+  canvas.height = size;
+  const sx = (video.videoWidth - size) / 2;
+  const sy = (video.videoHeight - size) / 2;
+  const sw = size;
+  const sh = size;
+  const dx = 0;
+  const dy = 0;
+  const dw = size;
+  const dh = size;
+  ctx.drawImage(video, sx, sy, sw, sh, dx, dy, dw, dh);
+  const capture = new Image(size, size);
   const blob = await new Promise<Blob | null>((resolve) => canvas.toBlob(resolve));
   if (!blob) throw new Error('Failed to create blob');
   capture.src = URL.createObjectURL(blob);
