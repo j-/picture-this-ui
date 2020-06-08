2dfa90d193109b77ac7d0e40bc6022ce33a252b2
j- <j@skeoh.com>
2020-06-08 15:14:12 +1000

    Rate limit capturing

--- src/components/ButtonCapture.tsx
+++ src/components/ButtonCapture.tsx
@@ -9,18 +9,32 @@ export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 const ButtonCapture: React.FC<Props> = ({ onTouchStart, ...props }) => {
   const dispatch = useDispatch();
   const video = useVideo();
+  const [disabled, setDisabled] = React.useState(false);
 
-  const handleTouchStart: React.TouchEventHandler<HTMLButtonElement> = async (e) => {
+  const handleTouchStart = React.useCallback<React.TouchEventHandler<HTMLButtonElement>>(async (e) => {
+    if (disabled) return;
     e.preventDefault();
     try {
       if (!video) throw new Error('Expected video element');
       await dispatch(capture(video));
     } catch (err) {}
+    setDisabled(true);
     if (onTouchStart) onTouchStart(e);
-  };
+  }, [disabled, dispatch, video, onTouchStart]);
+
+  React.useEffect(() => {
+    if (!disabled) return;
+    const timeout = setTimeout(() => setDisabled(false), 200);
+    return () => clearTimeout(timeout);
+  }, [disabled]);
 
   return (
-    <button className="ButtonCapture" onTouchStart={handleTouchStart} {...props} />
+    <button
+      className="ButtonCapture"
+      onTouchStart={handleTouchStart}
+      disabled={disabled}
+      {...props}
+    />
   );
 };
 
